59ae1b3b44fee7b142aeb789c20c47699db58cb4
okashehserag-dotcom <okashehserag@gmail.com>
2026-01-21 16:19:33 +0300

    1

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-
-
 const images = [
   "images/soon.png",
   "images/soon.png",
   "images/soon.png"
 ];
 
-let current = 0;
+let index = 0;
+let likes = Number(localStorage.getItem("likes")) || 0;
+
 const slide = document.getElementById("slide");
+const likesBox = document.getElementById("likes");
+
+if (likesBox) likesBox.textContent = likes;
 
 function next() {
-  current = (current + 1) % images.length;
-  slide.src = images[current];
+  index = (index + 1) % images.length;
+  slide.src = images[index];
 }
 
 function prev() {
-  current = (current - 1 + images.length) % images.length;
-  slide.src = images[current];
+  index = (index - 1 + images.length) % images.length;
+  slide.src = images[index];
+}
+
+function like() {
+  likes++;
+  localStorage.setItem("likes", likes);
+  likesBox.textContent = likes;
 }
